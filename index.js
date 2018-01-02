c78ccc2f728a883a7aa84dd1272e5f004cbe9b34
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-02 11:59:01 -0500

    new login page

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux';
+// import store from './redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-AppRegistry.registerComponent('myMapApp', () => App);
+let store = createStore(rootReducer);
+
+
+export default class myMapApp extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+AppRegistry.registerComponent('myMapApp', () => myMapApp);
+
+
+
