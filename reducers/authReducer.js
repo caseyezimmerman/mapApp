18bd93bc6cd90ed562cdecc5f846af46a5c84d8d
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-02 23:07:31 -0500

    idk

--- reducers/authReducer.js
+++ reducers/authReducer.js
@@ -1,24 +1,40 @@
-const defaultState = {
-    isLoggedIn: false,
-    username: '',
-    password: ''
-};
+// const defaultState = {
+//     isLoggedIn: false,
+//     username: '',
+//     password: ''
+// };
 
-export default function reducer(state = defaultState, action) {
-    switch (action.type) {
-        case 'LOGIN': 
-            return Object.assign({}, state, { 
-                isLoggedIn: true,
-                username: action.username,
-                password: action.password
-            });
-        case 'LOGOUT':
-            return Object.assign({}, state, { 
-                isLoggedIn: false,
-                username: '',
-                password: ''
-            });
-        default:
-            return state;
-    }
+// export default function reducer(state = defaultState, action) {
+//     switch (action.type) {
+//         case 'LOGIN': 
+//             return Object.assign({}, state, { 
+//                 isLoggedIn: true,
+//                 username: action.username,
+//                 password: action.password
+//             });
+//         case 'LOGOUT':
+//             return Object.assign({}, state, { 
+//                 isLoggedIn: false,
+//                 username: '',
+//                 password: ''
+//             });
+//         default:
+//             return state;
+//     }
+// }
+
+function reducer (state=[], action){
+	if(action.type === 'LOGIN'){
+		// im going to update
+		return action.payload.data
+	}else if (action.type === 'LOGOUT'){
+		return []
+	}else{
+		// i dont care about this action. just return state
+		return state
+	}
+	
+	
 }
+
+export default reducer
