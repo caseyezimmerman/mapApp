e98bf354809fe936ee141f36b4aafb6f4b6b184b
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-03 17:58:02 -0500

    map

--- components/MapMaker.js
+++ components/MapMaker.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity
+} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 class MapMaker extends Component{
-  render(){
-    return(
-      <Text>Hello</Text>
-    )
+    constructor(props) {
+      super(props);
+        this.state = {
+        coordinate: new MapView.AnimatedRegion({
+          latitude: 37.78825,
+          longitude: -122.4324,
+        }),
+      };
+    }
+
+    render() {
+    const { region } = this.props;
+    console.log(this.props)
+    return (
+      <View style ={styles.container}>
+        <Text style={styles.text}></Text>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        <MapView.Marker
+            coordinate={{latitude: 37.78825,
+            longitude: -122.4324}}
+            title={"title"}
+            description={"description"}
+         />
+        </MapView>
+      </View>
+    );
   }
 }
 export default MapMaker
+
+const styles = StyleSheet.create({
+  container: {
+    // ...StyleSheet.absoluteFillObject,
+    // height: 400,
+    // width: 400,
+    flex: 0.3,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+},
+  map: {
+    // ...StyleSheet.absoluteFillObject,
+
+    height:'200%',
+    width: '90%',
+    marginBottom: 20
+
+},
+  text:{
+    fontSize:20,
+    marginTop:20,
+
+  }
+
+});
