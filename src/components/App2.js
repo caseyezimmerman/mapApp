1a3eb10563e0047962c1106d01adc3a87d2879b8
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-04 16:34:05 -0500

    ??

--- src/components/App2.js
+++ src/components/App2.js
@@ -59,7 +59,41 @@ class App extends Component{
         longitude: -122.4324,
       })
     };
+    // this.getWaypoints = this.getWaypoints.bind(this)
   }
+
+  // getWaypoints(){
+        // var latArray = []
+        // var lngArray = []
+        // var markerArray = []
+        // var angle = 90
+        // var numOfPoints = 6;
+        // var degreesPerPoint = -5 /numOfPoints;
+        // var x2;
+        // var y2;
+
+        // for(let i=0; i <= numOfPoints; i++){
+        // x2 = Math.cos(angle) * range;
+        // y2 = Math.sin(angle) * range;
+        // newLat = lat+x2;
+        // newLng = lng+y2;
+        // // console.log(typeof newLat);
+        // // console.log(newLng)
+
+        // lat_lng = new google.maps.LatLng(newLat,newLng);
+        // var marker = new google.maps.Marker({
+        //   position: lat_lng,
+        //   map: map,
+        //   // visibile: false
+          
+        // });
+        
+        // latArray.push(lat_lng.lat()); ////push lats of points we just looped through and placed on map
+        // lngArray.push(lat_lng.lng());
+        // markerArray.push(marker);
+        // angle += degreesPerPoint;
+      
+  // }
 
   makeMap(e,navigator){
     e.preventDefault()
@@ -69,9 +103,7 @@ class App extends Component{
     distance = (this.refs.form.getComponent('distance').props.value)
     console.log(typeof currentLocation)
     this.props.mapAction(currentLocation,distance,navigator)
-
-
-
+    // getWaypoints()
   }
 
 
--- src/components/MapMaker.js
+++ src/components/MapMaker.js
@@ -31,7 +31,46 @@ class MapMaker extends Component{
       //   );
       // }
 
+    getWaypoints(){
+        var latArray = []
+        var lngArray = []
+        var markerArray = []
+        var angle = 90
+        var numOfPoints = 6;
+        var degreesPerPoint = -5 /numOfPoints;
+        var x2;
+        var y2;
+        var range = 0.11
+        var lat = userLat
+        var lng = userLng
+
+        for(let i=0; i <= numOfPoints; i++){
+        x2 = Math.cos(angle) * range;
+        y2 = Math.sin(angle) * range;
+        newLat = lat+x2;
+        newLng = lng+y2;
+        // console.log(typeof newLat);
+        // console.log(newLng)
+
+        lat_lng = new google.maps.LatLng(newLat,newLng);
+        var marker = new google.maps.Marker({
+          position: lat_lng,
+          map: map,
+          // visibile: false
+          
+        });
+        
+        latArray.push(lat_lng.lat()); ////push lats of points we just looped through and placed on map
+        lngArray.push(lat_lng.lng());
+        markerArray.push(marker);
+        angle += degreesPerPoint;
+
+
+      }
+    }
+
     render() {
+    getWaypoints()
     const { region } = this.props;
     console.log(this.props)
     if(this.props.theMap.userLatLng === undefined){
